8a780eccb6eb6cb0a7ca4a719752b82eaa20c29d
Bernardo Abella Moschen <bernardo.moschen@meta.com.br>
2021-05-21 12:33:05 -0300

    add files: exercise_1 to exercise_5

---
+++ freeCodeCamp/exercise_1.js
@@ -0,0 +1,9 @@
+///accessing-object-properties-with-dot-notation
+var testObj = {
+  "hat": "ballcap",
+  "shirt": "jersey",
+  "shoes": "cleats"
+};
+
+var hatValue = testObj.hat;      // Change this line
+var shirtValue = testObj.shirt;    // Change this line
