f5672bfe96704d35824a2207eb6fb9556f9b495b
BernardoMoschen <bernardomoschen.main@gmail.com>
2021-05-21 14:35:41 -0300

    Add files: exercise_1 to exercise_19

---
+++ freeCodeCamp/exercise_9.js
@@ -0,0 +1,24 @@
+var myMusic = [
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    },
+    {
+      "artist": "The Rolling Stones",
+      "title": "Simpathy for the Devil",
+      "release_year": 1968,
+      "formats": [
+        "CD",
+        "Cassete",
+        "LP"
+      ],
+      "gold": true
+    }, 
+  ];
