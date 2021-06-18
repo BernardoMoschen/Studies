307f53fad7e2b9b2cc0129001bdbf8aa05d13f9e
BernardoMoschen <bernardomoschen.dev@gmail.com>
2021-06-17 23:22:54 -0300

    Refactor: form.js, create functions: montaTr, montaTd, obtemPacienteDoFOrmulario

--- introducao-javascript/js/form.js
+++ introducao-javascript/js/form.js
@@ -3,35 +3,59 @@
 var botaoAdicionar = document.querySelector("#adicionar-paciente")
 botaoAdicionar.addEventListener("click", (event) => {
     event.preventDefault()
-    console.log("Eu fui clicado!")
     let form = document.querySelector("#form-adiciona")
 
-    var nome = form.nome.value
-    var peso = form.peso.value
-    var altura = form.altura.value
-    var gordura = form.gordura.value
+// Extrai informações do paciente do form
+    var paciente = obtemPacienteDoFormulario(form)
 
-    var pacienteTr = document.createElement("tr")
+// Cria a Tr e a Td do paciente
+    montaTr(paciente)
 
-    var nomeTd = document.createElement("td")
-    var pesoTd = document.createElement("td")
-    var alturaTd = document.createElement("td")
-    var gorduraTd = document.createElement("td")
-    var imcTd = document.createElement("td")
-
-    nomeTd.textContent = nome
-    pesoTd.textContent = peso
-    alturaTd.textContent = altura
-    gorduraTd.textContent = gordura
-    imcTd.textContent = calculaImc(peso,altura)
-
-    pacienteTr.appendChild(nomeTd)
-    pacienteTr.appendChild(pesoTd)
-    pacienteTr.appendChild(alturaTd)
-    pacienteTr.appendChild(gorduraTd)
-    pacienteTr.appendChild(imcTd)
-
+// Adiciona o paciente na tabela
     var tabela = document.querySelector("#tabela-pacientes")
 
     tabela.appendChild(pacienteTr)
 })
+
+// Função p/ obter dados do paciente obtidos pelo formulário
+var  obtemPacienteDoFormulario = (form) => {
+
+    var paciente = {
+        nome: form.name.value,
+        peso: form.name.value,
+        altura: form.name.value,
+        gordura: form.name.value,
+        imc: calculaImc(form.peso.value, form.altura.value)
+    }
+
+    return pacientes
+}
+
+// Função p/ montar uma Tr com os dados do paciente
+var montaTr = (paciente) => {
+
+    var pacienteTr = document.createElement("tr")
+    pacienteTr.classList.add("paciente")
+
+    var nomeTd = montaTd(paciente.nome, "info-nome")
+    var pesoTd = montaTd(paciente.peso, "info-peso")
+    var alturaTd = montaTd(paciente.altura, "info-altura")
+    var gorduraTd = montaTd(paciente.gordura, "info-gordura")
+    var imcTd = montaTd(paciente.imc, "info-imc")
+
+    pacienteTr.appendChild(pesoTd)
+    pacienteTr.appendChild(alturaTd)
+    pacienteTr.appendChild(nomeTd)
+    pacienteTr.appendChild(gorduraTd)
+    pacienteTr.appendChild(imcTd)
+
+}
+
+// Função p/ montar uma Td com os dados do paciente
+var montaTd = (dado, classe) => {
+
+    var td = document.createElement("td")
+    td.textContent = dado
+    td.classList.add(classe)
+    return td
+}
