6a0724b967095836e10c098f071e30545d043fe5
BernardoMoschen <bernardomoschen.dev@gmail.com>
2021-06-20 22:24:11 -0300

    feat: improve pacient filter

--- introducao-javascript/js/filtra.js
+++ introducao-javascript/js/filtra.js
@@ -1,14 +1,22 @@
 var campoFiltro = document.querySelector("#filtrar-tabela");
 
 campoFiltro.addEventListener("input", function() {
-    let pacientes = document.querySelectorAll(".paciente");
+    var pacientes = document.querySelectorAll(".paciente");
 
     if (this.value.length > 0) {
-        for (let paciente of pacientes) {   
-            paciente.classList.remove("invisivel");    
+        for (let paciente of pacientes) {
+            var nomePacienteTd = paciente.querySelector(".info-nome").textContent
+            var expressao = new RegExp(this.value, "i");
+
+            if (expressao.test(nomePacienteTd)) {
+                paciente.classList.remove("invisivel");
+            } else {
+                paciente.classList.add("invisivel");
+            }
         }
-    }
-    for (let paciente of pacientes) {
-        paciente.classList.add("invisivel");
+    } else {
+        for (let paciente of pacientes) {
+            paciente.classList.remove("invisivel");
+        }
     }
 });
