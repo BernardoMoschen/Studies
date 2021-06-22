0ab525808e23e29e4bd068066b0cf2d420c3b763
BernardoMoschen <bernardomoschen.dev@gmail.com>
2021-06-21 22:34:26 -0300

    First commit

---
+++ Vuejs/alurapic/src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class='corpo'>
+    <h1 class='centralizado'>{{ titulo }}</h1>
+
+    <ul class="lista-fotos">
+      <li class='lista-fotos-item' v-for="foto of fotos">
+
+        <meu-painel :titulo="foto.titulo">
+          <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo">
+        </meu-painel>
+      </li>
+    </ul>
+
+  </div>
+</template>
+
+<script>
+export default {
+
+  data() {
+
+    return {
+
+      titulo: 'Alurapic',
+      fotos: []
+    }
+  },
+
+  created() {
+
+    this.$http.get('http://localhost:3000/v1/fotos')
+      .then(res => res.json())
+      .then(fotos => this.fotos = fotos, err => console.log(err));
+  }
+}
+
+</script>
+
+<style>
+
+.centralizado {
+  text-align: center;
+}
+
+.corpo {
+  font-family: Helvetica, sans-serif;
+  margin: 0 auto;
+  width: 96%;
+}
+
+.lista-fotos {
+  list-style: none;
+}
+
+.lista-fotos .lista-fotos-item {
+  display: inline-block;
+  margin: 15px;
+}
+
+</style>
---
+++ Vuejs/alurapic/src/assets/components/shared/painel/Painel.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class='corpo'>
+    <h1 class='centralizado'>{{ titulo }}</h1>
+
+    <ul class="lista-fotos">
+        <h2 class="painel-titulo">{{ foto.titulo }}</h2>
+        <div class="painel-conteudo">
+        </div>
+      </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+
+</style>
