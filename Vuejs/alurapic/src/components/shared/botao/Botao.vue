33b2a5ecd2a8d43657fd904faf27ad5e5698dbe9
BernardoMoschen <bernardomoschen.dev@gmail.com>
2021-06-23 12:41:11 -0300

    feat: add property validation

--- Vuejs/alurapic/src/components/shared/botao/Botao.vue
+++ Vuejs/alurapic/src/components/shared/botao/Botao.vue
@@ -1,11 +1,33 @@
+
 <template>
     <button :class="estiloDoBotao" :type="tipo" @click="disparaAcao()">{{rotulo}}</button>
 </template>
 <script>
 export default {
 
-   props: ['tipo', 'rotulo', 'confirmação', 'estilo'],
+   props: {
+            tipo: {
+                required: true,
+                type: String
+            },
 
+            rotulo: {
+                required: true,
+                type: String
+            },
+
+            confirmacao: {
+                required: false,
+                default: false,
+                type: Boolean
+            },
+
+            estilo: {
+                required: false,
+                default: 'padrao',
+                type: String
+            }
+        },
    methods: {
 
        disparaAcao() {
