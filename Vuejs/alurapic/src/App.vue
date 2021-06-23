8303f0187bac5e7a34309beef1314431bccdd686
BernardoMoschen <bernardomoschen.dev@gmail.com>
2021-06-22 22:35:02 -0300

    feat: add menu to navigate between routes

--- Vuejs/alurapic/src/App.vue
+++ Vuejs/alurapic/src/App.vue
@@ -1,20 +1,42 @@
+<!-- alurapic/src/App.vue -->
+
 <template>
   <div class="corpo">
-    <router-view></router-view>
+
+    <nav>
+      <ul>
+          <li v-for="route in routes">
+            <router-link :to="route.path ? route.path : '/'">{{route.titulo}}</router-link>
+          </li>
+      </ul>
+    </nav>
+
+     <router-view></router-view>
+
   </div>
 </template>
-
 <script>
+
+import { routes }  from './routes';
+
 export default {
 
+  data() {
+
+    return {
+
+      routes
+    }
+
+  }
+
 }
-
 </script>
-
 <style>
+
   .corpo {
     font-family: Helvetica, sans-serif;
-    width: 96%;
     margin: 0 auto;
+    width: 96%;
   }
 </style>
