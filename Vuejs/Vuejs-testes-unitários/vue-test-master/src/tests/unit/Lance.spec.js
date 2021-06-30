0d1afdaa9e3befaeccb196ac2ebb0c46114129e4
BernardoMoschen <bernardomoschen.dev@gmail.com>
2021-06-30 10:35:00 -0300

    feat: finish test case

--- Vuejs/Vuejs-testes-unitários/vue-test-master/src/tests/unit/Lance.spec.js
+++ Vuejs/Vuejs-testes-unitários/vue-test-master/src/tests/unit/Lance.spec.js
@@ -23,4 +23,23 @@ describe('Um lance sem valor minimo', () => {
     expect(lancesEmitidos).toBeUndefined()
   })
 
-  
+  test('emite um lance quando o valor for maio que zero', () => {
+    const wrapper = mount(Lance)
+    const input = wrapper.find('input')
+    input.setValue(100)
+    wrapper.trigger('submit')
+    const lancesEmitidos = wrapper.emitted('novo-lance')
+    expect(lancesEmitidos).toHaveLength(1)
+  })
+
+  test('emite o valor esperado de um lance válido', () => {
+    const wrapper = mount(Lance)
+    const input = wrapper.find('input')
+    input.setValue(100)
+    wrapper.trigger('submit')
+    const lancesEmitidos = wrapper.emitted('novo-lance')
+    const lance = parseInt(lancesEmitidos[0][0])
+    expect(lance).toBe(100)
+  })
+})
+
