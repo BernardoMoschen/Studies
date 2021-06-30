13a33cd81d708d56394dc046f2190ce5534e701e
BernardoMoschen <bernardomoschen.dev@gmail.com>
2021-06-30 10:37:14 -0300

    add another test scenario

--- Vuejs/Vuejs-testes-unitários/vue-test-master/src/tests/unit/Lance.spec.js
+++ Vuejs/Vuejs-testes-unitários/vue-test-master/src/tests/unit/Lance.spec.js
@@ -43,3 +43,31 @@ describe('Um lance sem valor minimo', () => {
   })
 })
 
+describe('Um lance com valor minimo', () => {
+  test('todos os lances devem possuir valor maior que o minimo informado', () => {
+    const wrapper = mount(Lance, {
+      propsData: {
+        lanceMinimo: 300
+      }
+    })
+    const input = wrapper.find('input')
+    input.setValue(400)
+    wrapper.trigger('submit')
+    const lancesEmitidos = wrapper.emitted('novo-lance')
+    expect(lancesEmitidos).toHaveLength(1)
+  })
+
+  test('emite o valor esperado de um lance valido', () => {
+    const wrapper = mount(Lance, {
+      propsData: {
+        lanceMinimo: 300
+      }
+    })
+    const input = wrapper.find('input')
+    input.setValue(400)
+    wrapper.trigger('submit')
+    const lancesEmitidos = wrapper.emitted('novo-lance')
+    const valorDoLance = parseInt(lancesEmitidos[0][0])
+    expect(valorDoLance).toBe(400)
+  })
+})
