8f3388cbee60a7c10f6aa99d09630e0e7e3c0135
BernardoMoschen <bernardomoschen.dev@gmail.com>
2021-06-30 10:34:10 -0300

    feat: add more test cases

--- Vuejs/Vuejs-testes-unitários/vue-test-master/src/tests/unit/Lance.spec.js
+++ Vuejs/Vuejs-testes-unitários/vue-test-master/src/tests/unit/Lance.spec.js
@@ -1,7 +1,26 @@
 import Lance from '@/components/Lance'
 import { mount } from '@vue/test-utils'
 
-test('não aceita lance com valor menor que zero', () => {
-  const wrapper = mount(Lance)
-  expect(wrapper).toBeTruthy()
-})
+
+describe('Um lance sem valor minimo', () => {
+  test('verifica se há wrapper', () =>{
+    const wrapper = mount(Lance)
+    expect(wrapper).toBeTruthy()
+  })
+
+  test('verifica se o input existe', () => {
+    const wrapper = mount(Lance)
+    const input = wrapper.find('input')
+    expect(input).toBeTruthy()
+  })
+
+  test('não aceita lance com valor menor que zero', () => {
+    const wrapper = mount(Lance)
+    const input = wrapper.find('input')
+    input.setValue(-100)
+    wrapper.trigger('submit')
+    const lancesEmitidos = wrapper.emitted('novo-lance')
+    expect(lancesEmitidos).toBeUndefined()
+  })
+
+  
