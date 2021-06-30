67c1c045a6d8caf600073d17e6aa65b226dbfa5a
BernardoMoschen <bernardomoschen.dev@gmail.com>
2021-06-29 21:56:55 -0300

    feat: add lazy routes and hook for routes

--- Vuejs/Auth_Vuex/Vuejs_Auth_Vuex/src/router/index.js
+++ Vuejs/Auth_Vuex/Vuejs_Auth_Vuex/src/router/index.js
@@ -1,9 +1,6 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Gerentes from '../views/Gerentes.vue'
-import Home from '../views/Home.vue'
-import NovoUsuario from '../views/NovoUsuario.vue'
-import Login from '../views/Login.vue'
+import VueRouter from 'vue-router' 
+import provedor from '@/provedor.js'
 
 Vue.use(VueRouter)
 
@@ -11,27 +8,47 @@ const routes = [
   {
     path: '',
     name: 'home',
-    component: Home
+    component: () => import ( /*webpackChunkName: Home */
+      '../views/Home.vue'),
+      meta: {
+        publica : true
+      }
   },
   {
     path: '/gerentes',
     name: 'gerentes',
-    component: Gerentes
+    component: () => import ( /*webpackChunkName: Home */
+      '../views/Gerentes.vue'),
   },
   {
     path: '/cadastre-se',
     name: 'novo.usuario',
-    component: NovoUsuario
+    component: () => import ( /*webpackChunkName: Login */
+      '../views/NovoUsuario.vue'),
+    meta: {
+      publica : true
+    }
   },
   {
     path: '/login',
     name: 'login',
-    component: Login
+    component: () => import ( /*webpackChunkName: Registrar*/
+    '../views/Login.vue'),
+    meta: {
+      publica : true
+    }
   }
 ]
 
 const router = new VueRouter({
   routes
+})
+
+router.beforeEach((routeTo, routeFrom, next) => {
+  if (!routeTo.meta.publica && !provedor.state.token) {
+      return next({ path: '/login' })
+  }
+  next()
 })
 
 export default router
