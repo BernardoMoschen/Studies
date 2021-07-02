911e17c3dfa47330c0ad18dc32673e36561e3ea0
BernardoMoschen <bernardomoschen.dev@gmail.com>
2021-07-02 13:23:06 -0300

    First commit: php studies

---
+++ PHP/Fundamentos_PHP/variaveis.php
@@ -0,0 +1,19 @@
+<?php
+
+/* Declaração de variáveis
+
+=> Iniciam com " $ "
+=> O php é dinamicamente tipado, logo, não necessita de declaração do tipo da variavel
+Exemplos:
+*/
+
+$idade = 25;
+$salario = 1000.30;
+$divisao = 10 / 2;
+$multiplicacao = 5 * 2;
+$exponenciacao = 7 ** 3;
+$resto = 11 % 2;
+$verdadeiro = true;
+$falso = false;
+
+echo gettype($verdadeiro);
