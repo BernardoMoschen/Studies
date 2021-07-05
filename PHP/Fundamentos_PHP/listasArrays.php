cd3c4e688a1f92faec292243b7da5d008b41e891
BernardoMoschen <bernardomoschen.dev@gmail.com>
2021-07-05 19:19:21 -0300

    docs: add listasArrays to Fundamentos_PHP

---
+++ PHP/Fundamentos_PHP/listasArrays.php
@@ -0,0 +1,9 @@
+<?php
+
+// Declaração:
+
+$listaIdades = [23, 19, 25, 30, 41, 18, 21];
+//ou: $listaIdades = array(23, 19, 25, 30, 41, 18, 21);
+
+// Contar a quantidade de intens em uma lista/array:
+echo count ($listaIdades); // = 7
