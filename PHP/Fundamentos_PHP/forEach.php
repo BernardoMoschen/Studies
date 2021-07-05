02745921facbdd1bc7101adb2f86eaa62fcf83fd
BernardoMoschen <bernardomoschen.dev@gmail.com>
2021-07-05 20:20:44 -0300

    refactor: change te accounts index

--- PHP/Fundamentos_PHP/forEach.php
+++ PHP/Fundamentos_PHP/forEach.php
@@ -3,22 +3,22 @@
 // O loop forEach permite iterar sem iniciar um contador e sem saber o valor dos índices
 
 $contasCorrentes = [
-    123989812 => [
+    11111=> [
         'titular' => 'Bernardo', // Chave = 'titular', valor do índice = 'Bernardo'
         'saldo' => 1000 // Chave = 'saldo', valor do índice = '1000'
     ],
-    12039801923 => [
+    11112 => [
         'titular' => 'Maria', // Chave = 'titular', valor do índice = 'Maria'
         'saldo' => 9875 // Chave = 'saldo', valor do índice = '9875'
     ],
-    1238129321 => [
+    11113 => [
         'titular' => 'Adalberto', // Chave = 'titular', valor do índice = 'Adalberto'
         'saldo' => 4500 // Chave = 'saldo', valor do índice = '4500'
     ]
 ];
 
 // Utiização do forEach
-foreach ($contasCorrentes as $cpf => $conta){ // Atribuindo o nome do índice a váriavel $cpf
+foreach ($contasCorrents as $cpf => $conta){ // Atribuindo o nome do índice a váriavel $cpf
     echo $cpf . PHP_EOL; // Acessando o índice
     echo $conta['titular'] . PHP_EOL; // Acessando a o índice titular
 }
