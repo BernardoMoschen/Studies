f19ba170220acc402c8ba2837c2778c81919702f
BernardoMoschen <bernardomoschen.dev@gmail.com>
2021-07-05 20:02:20 -0300

    fix: $contasCorrentas → $contasCorrentes

--- PHP/Fundamentos_PHP/forEach.php
+++ PHP/Fundamentos_PHP/forEach.php
@@ -2,7 +2,7 @@
 
 // O loop forEach permite iterar sem iniciar um contador e sem saber o valor dos índices
 
-$contasCorrentas = [
+$contasCorrentes = [
     123989812 => [
         'titular' => 'Bernardo', // Chave = 'titular', valor do índice = 'Bernardo'
         'saldo' => 1000 // Chave = 'saldo', valor do índice = '1000'
@@ -18,7 +18,7 @@ $contasCorrentas = [
 ];
 
 // Utiização do forEach
-foreach ($contasCorrentas as $cpf => $conta){ // Atribuindo o nome do índice a váriavel $cpf
+foreach ($contasCorrentes as $cpf => $conta){ // Atribuindo o nome do índice a váriavel $cpf
     echo $cpf . PHP_EOL; // Acessando o índice
     echo $conta['titular'] . PHP_EOL; // Acessando a o índice titular
 }
