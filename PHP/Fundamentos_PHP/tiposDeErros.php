9d769ca9b2f24e86a1e1e85fe30b578cd99578fd
BernardoMoschen <bernardomoschen.dev@gmail.com>
2021-07-05 21:05:37 -0300

    docs: add tiposDeErros.php

---
+++ PHP/Fundamentos_PHP/tiposDeErros.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+
+=> E_NOTICE: PHP encontra um problema mas consegue 'se virar' e avisa o usuário.
+                ex: tenta realizar uma conversão.
+
+=> E_WARNING: PHP encontra um problema que não consegue resolver, mas continua a execução.
+                Além disso, ele avisa o usuário.
+
+=> E_ERROR: php encontra um problema crítico, impedindo a execução do código.
+                Além disso, ele avisa o usuário.
+
+*/
