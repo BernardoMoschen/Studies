8e1511c01c860c8e76883992aa0a9104c2475eae
BernardoMoschen <bernardomoschen.dev@gmail.com>
2021-07-28 20:14:48 -0300

    feat: add constructors

--- Javascript/javascriptPOO/bitebank/index.js
+++ Javascript/javascriptPOO/bitebank/index.js
@@ -1,42 +1,48 @@
 class Cliente {
     nome;
     cpf;
+    
+    constructor(nome, cpf) {
+        this.nome = nome;
+        this.cpf = cpf;
+    }
 }
 
 class contaCorrente {
-    saldo;
-    agencia;
+    #saldo;
+
+    constructor(agencia){
+        this.#saldo = 0
+        this.agencia = agencia;
+    }
 
     sacar(valor){
-        if(this.saldo >= valor) {
-            this.saldo -= valor;
+        if(this.#saldo >= valor) {
+            this.#saldo -= valor;
         }
-    };
+    }
 
     depositar(valor){
-        if(this.saldo >= valor) {
-            this.saldo += valor;
+        if(this.#saldo >= 0) {
+            this.#saldo += valor;
         }
-    };
+    }
 }
 
 const cliente1 = new Cliente();
-const cliente2 = new Cliente();
-const cliente3 = new Cliente();
-
-cliente1.nome = 'Bernardo';
+cliente1.nome = "Bernardo";
 cliente1.cpf = 11223344556;
 
-cliente2.nome = 'Marcelo';
-cliente2.cpf = 65544332211;
+console.log(cliente1)
+// const cliente2 = new Cliente();
+// cliente2.nome = 'Marcelo';
+// cliente2.cpf = 65544332211;
 
 const contaCorrenteBernardo = new contaCorrente;
 contaCorrenteBernardo.agencia = 1001;
-contaCorrenteBernardo.saldo = 0;
 
+// const contaCorrenteMarcelo = new contaCorrente;
+// contaCorrenteBernardo.agencia = 1001;
 
-
-const contaCorrenteMarcelo = new contaCorrente;
-contaCorrenteBernardo.agencia = 1001;
-contaCorrenteBernardo.saldo = 0;
-
+contaCorrenteBernardo.depositar(100);
+console.log(contaCorrenteBernardo);
