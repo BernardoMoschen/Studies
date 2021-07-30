9fb49eaaf28c148743fa2b1f6255cb55bde5d0d6
BernardoMoschen <bernardomoschen.dev@gmail.com>
2021-07-29 23:51:56 -0300

    refactor: cleans code (delete console.log)

--- Javascript/javascriptPOO/bitebank/index.js
+++ Javascript/javascriptPOO/bitebank/index.js
@@ -3,16 +3,12 @@ import { ContaCorrente } from "./ContaCorrente.js";
 
 
 const cliente1 = new Cliente('Bernardo', 11223344556);
-console.log(cliente1);
-
 const cliente2 = new Cliente('Marcelo', 65544332211);
-console.log(cliente2);
+
 
 const cC1 = new ContaCorrente(1001, cliente1);
-cC1.depositar(100);
+const cC2 = new ContaCorrente(1001, cliente2);
 
-const cC2 = new ContaCorrente(1001, 'Alberto');
 cC2.depositar(500);
 cC2.transferir(200, cC1);
-
 
