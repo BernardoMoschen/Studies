eda67047c2ea43020ed0db8cbf6c98d358419098
BernardoMoschen <bernardomoschen.dev@gmail.com>
2021-07-30 19:21:25 -0300

    feat: add a specifc sacar to cC

--- Javascript/javascriptPOO/bitebank/Contas/ContaCorrente.js
+++ Javascript/javascriptPOO/bitebank/Contas/ContaCorrente.js
@@ -7,4 +7,12 @@ export class ContaCorrente extends Conta{
         super(cliente, agencia, 0);
         ContaCorrente.numeroDeContas += 1;
     }
+
+    sacar(valor) {
+        let taxa = 1.1;
+        const valorSacado = taxa * valor;
+        if(Number.parseFloat(valorSacado) && this.saldo >= valorSacado) {
+            this.saldo -= valorSacado.toFixed(2);
+        }
+    }
 }
--- Javascript/javascriptPOO/bitebank/index.js
+++ Javascript/javascriptPOO/bitebank/index.js
@@ -1,6 +1,6 @@
-import { Cliente } from "./Cliente.js";
-import { ContaCorrente } from "./ContaCorrente.js";
-import { ContaPoupanca } from "./ContaPoupanca.js";
+import { Cliente } from "./Clientes/Cliente.js";
+import { ContaCorrente } from "./Contas/ContaCorrente.js";
+import { ContaPoupanca } from "./Contas/ContaPoupanca.js";
 
 const cliente1 = new Cliente('Bernardo', 11223344556);
 const cliente2 = new Cliente('Marcelo', 665544332211);
@@ -9,7 +9,8 @@ const cC1 = new ContaCorrente(cliente1, 1001);
 cC1.depositar(700);
 cC1.sacar(400);
 
-const cP1 = new ContaPoupanca(cliente2, 2525, 500);
+const cP1 = new ContaPoupanca(cliente2, 2525, 700);
+cP1.sacar(400);
 
 cC1.exibeDados();
 cP1.exibeDados();
