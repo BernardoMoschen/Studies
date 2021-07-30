c385aa6e36104243dab043db4862e05db0b1674c
BernardoMoschen <bernardomoschen.dev@gmail.com>
2021-07-29 23:14:52 -0300

    refactor: change contaCorrente to cC

--- Javascript/javascriptPOO/bitebank/index.js
+++ Javascript/javascriptPOO/bitebank/index.js
@@ -7,19 +7,25 @@ cliente1.nome = "Bernardo";
 cliente1.cpf = 11223344556;
 console.log(cliente1);
 
-const contaCorrenteBernardo = new contaCorrente;
-contaCorrenteBernardo.agencia = 1001;
-contaCorrenteBernardo.cliente = cliente1;
-contaCorrenteBernardo.depositar(100);
-console.log(contaCorrenteBernardo);
+const cC1 = new contaCorrente;
+cC1.agencia = 1001;
+cC1.cliente = cliente1;
+cC1.depositar(100);
+console.log(cC1);
+
 
 const cliente2 = new Cliente();
 cliente2.nome = 'Marcelo';
-cliente2.cpf = 65544332211
+cliente2.cpf = 65544332211;
 console.log(cliente2);
 
-const contaCorrenteMarcelo = new contaCorrente;
-contaCorrenteMarcelo.agencia = 1001
-contaCorrenteMarcelo.cliente = cliente2;
-contaCorrenteMarcelo.depositar(500);
-console.log(contaCorrenteMarcelo)
+const cC2 = new contaCorrente;
+cC2.agencia = 1001;
+cC2.cliente = cliente2;
+cC2.depositar(500);
+console.log(cC2);
+
+cC2.transferir(200, cC1);
+
+cC1.exibeSaldo();
+cC2.exibeSaldo();
