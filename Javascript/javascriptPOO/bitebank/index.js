ad94ff0b8421b702825019559e18b89a5692f19e
BernardoMoschen <bernardomoschen.dev@gmail.com>
2021-07-30 20:39:27 -0300

    feat: create class 'Diretor' and 'Gerente'

--- Javascript/javascriptPOO/bitebank/index.js
+++ Javascript/javascriptPOO/bitebank/index.js
@@ -1,20 +1,30 @@
 import { Cliente } from "./Clientes/Cliente.js";
-import { Conta } from "./Contas/Conta.js";
-import { ContaCorrente } from "./Contas/ContaCorrente.js";
-import { ContaPoupanca } from "./Contas/ContaPoupanca.js";
-
-const cliente1 = new Cliente('Bernardo', 11223344556);
-const cliente2 = new Cliente('Marcelo', 665544332211);
-
-const cC1 = new ContaCorrente(cliente1, 1001);
-cC1.depositar(700);
-cC1.sacar(400);
-
-const cP1 = new ContaPoupanca(cliente2, 2525, 700);
-cP1.sacar(400);
-
-cC1.exibeDados();
-cP1.exibeDados();
+import { Gerente } from "./Funcionários/Gerente.js";
+import { Diretor } from "./Funcionários/Gerente.js";
 
 
-console.log(Conta.numeroDeContas)
+const diretor1 = new Diretor("Rodrigo", 1000, 778899112233)
+const geretir1 = new Diretor("Alberto", 500, 112233445566)
+
+
+
+
+// import { Conta } from "./Contas/Conta.js";
+// import { ContaCorrente } from "./Contas/ContaCorrente.js";
+// import { ContaPoupanca } from "./Contas/ContaPoupanca.js";
+
+// const cliente1 = new Cliente('Bernardo', 11223344556);
+// const cliente2 = new Cliente('Marcelo', 665544332211);
+
+// const cC1 = new ContaCorrente(cliente1, 1001);
+// cC1.depositar(700);
+// cC1.sacar(400);
+
+// const cP1 = new ContaPoupanca(cliente2, 2525, 700);
+// cP1.sacar(400);
+
+// cC1.exibeDados();
+// cP1.exibeDados();
+
+
+// console.log(Conta.numeroDeContas)
