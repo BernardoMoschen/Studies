b9c7434399b8b5ec948d48d53eb415324591eb82
BernardoMoschen <bernardomoschen.dev@gmail.com>
2021-07-30 19:06:38 -0300

    refactor: delete unnecessary import

--- Javascript/javascriptPOO/bitebank/index.js
+++ Javascript/javascriptPOO/bitebank/index.js
@@ -1,15 +1,13 @@
 import { Cliente } from "./Cliente.js";
 import { ContaCorrente } from "./ContaCorrente.js";
 import { ContaPoupanca } from "./ContaPoupanca.js";
-import { Conta } from "./Conta.js";
-
 
 const cliente1 = new Cliente('Bernardo', 11223344556);
 const cliente2 = new Cliente('Marcelo', 665544332211);
 
 const cC1 = new ContaCorrente(cliente1, 1001);
 cC1.depositar(700);
-cC1.sacar(200);
+cC1.sacar(400);
 
 const cP1 = new ContaPoupanca(cliente2, 2525, 500);
 
