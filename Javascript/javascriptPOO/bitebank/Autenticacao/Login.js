8b53cbd24143b0344a51ff870717e163063aa7cf
BernardoMoschen <bernardomoschen.dev@gmail.com>
2021-07-30 22:27:24 -0300

    refactor: better login

--- Javascript/javascriptPOO/bitebank/Autenticacao/Login.js
+++ Javascript/javascriptPOO/bitebank/Autenticacao/Login.js
@@ -1,5 +1,12 @@
 export class SistemaAutenticacao {
     static login(autenticavel, senha) {
-       return autenticavel.autenticar(senha);
+        if(SistemaAutenticacao.temAutentificacao(autenticavel)) {
+            return autenticavel.autenticar(senha);
+        }
+        return false
+    };
+
+    static temAutentificacao(autenticavel) {
+        return "autenticar" in autenticar && autenticavel.autenticar instanceof Function;
     }
 }
