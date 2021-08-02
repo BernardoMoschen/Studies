22911721cd70c437f73212477af08b3838538157
BernardoMoschen <bernardomoschen.dev@gmail.com>
2021-08-02 09:01:13 -0300

    feat: adds css to components

--- ReactJS/FormaçaoReactJs/ceep/src/components/ListaDeNotas/ListaDeNotas.jsx
+++ ReactJS/FormaçaoReactJs/ceep/src/components/ListaDeNotas/ListaDeNotas.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import CardNota from "./CardNota/CardNota";
+import CardNota from "../CardNota/CardNota";
+import "./Style.css"
 
 export class ListaDeNotas extends Component {
   render() {
