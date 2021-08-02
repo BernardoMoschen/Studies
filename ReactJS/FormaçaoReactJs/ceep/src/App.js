73545462c57764da8a62dab8461f3de86879f98c
BernardoMoschen <bernardomoschen.dev@gmail.com>
2021-08-02 09:15:26 -0300

    refactor: refactor code

--- ReactJS/FormaçaoReactJs/ceep/src/App.js
+++ ReactJS/FormaçaoReactJs/ceep/src/App.js
@@ -7,16 +7,11 @@ import './assets/index.css';
 export class App extends Component {
   render() {
     return (
-      <section>
-      <form>
-        <input type="text" placeholder="Título" />
-        <textarea placeholder="Escreva sua nota..." />
-        <button>Criar nota</button>
-      </form>
-      <FormularioCadastro/>
-      <ListaDeNotas />
-    </section>
-    )
+      <section className="conteudo">
+        <FormularioCadastro />
+        <ListaDeNotas />
+      </section>
+    );
   }
 }
 
