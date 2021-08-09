07bfed6ffa0dfedef24a863014e014df5ed819d6
BernardoMoschen <bernardomoschen.dev@gmail.com>
2021-08-09 19:21:51 -0300

    feat: adds button to add a new client

---
+++ Javascript/javascriptCRUD/projeto_inicial/admin/controller/cadastraClientes_Controller.js
@@ -0,0 +1,12 @@
+import { criaCliente } from '../service/cliente_service.js';
+
+const formulario = document.querySelector('[data-form]');
+
+formulario.addEventListener('submit', (evento) => {
+  evento.preventDefault();
+  const nome = evento.target.querySelector('[data-nome]').value;
+  const email = evento.target.querySelector('[data-email]').value;
+
+  criaCliente(nome, email)
+    .then(() => { window.location.href = '../telas/cadastro_concluido.html'; });
+});
