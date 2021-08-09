a0d5c389d2e9f8acfd34206b3bc490abe8adb125
BernardoMoschen <bernardomoschen.dev@gmail.com>
2021-08-09 20:52:34 -0300

    feat(atualiza_cliente): now actually updates the client

--- Javascript/javascriptCRUD/projeto_inicial/admin/controller/atualizaCliente_Controller.js
+++ Javascript/javascriptCRUD/projeto_inicial/admin/controller/atualizaCliente_Controller.js
@@ -1,5 +1,5 @@
 // eslint-disable-next-line import/extensions
-import { detalhaCliente } from '../service/cliente_service.js';
+import { detalhaCliente, atualizaCliente } from '../service/cliente_service.js';
 
 const id = new URL(window.location).searchParams.get('id');
 const inputNome = document.querySelector('[data-nome]');
@@ -8,4 +8,12 @@ const inputEmail = document.querySelector('[data-email]');
 detalhaCliente(id).then((dados) => {
   inputNome.value = dados.nome;
   inputEmail.value = dados.email;
+});
+
+const formulario = document.querySelector('[data-form]');
+formulario.addEventListener('submit', (evento) => {
+  evento.preventDefault();
+  atualizaCliente(id, inputNome.value, inputEmail.value).then(() => {
+    window.location.href = '../telas/edicao_concluida.html';
+  });
 });
--- Javascript/javascriptCRUD/projeto_inicial/admin/service/cliente_service.js
+++ Javascript/javascriptCRUD/projeto_inicial/admin/service/cliente_service.js
@@ -16,6 +16,17 @@ const removeCLiente = (id) => fetch(`http://localhost:3000/profile/${id}`, {
 
 const detalhaCliente = (id) => fetch(`http://localhost:3000/profile/${id}`).then((resposta) => resposta.json());
 
+const atualizaCliente = (id, nome, email) => fetch(`http://localhost:3000/profile/${id}`, {
+  method: 'PUT',
+  headers: {
+    'Content-type': 'application/json',
+  },
+  body: JSON.stringify({
+    nome,
+    email,
+  }),
+}).then((resposta) => resposta.json());
+
 export {
-  listaClientes, criaCliente, removeCLiente, detalhaCliente,
+  listaClientes, criaCliente, removeCLiente, detalhaCliente, atualizaCliente,
 };
