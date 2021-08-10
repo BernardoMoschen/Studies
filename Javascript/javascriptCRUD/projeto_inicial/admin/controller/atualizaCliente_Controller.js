a3e0edb19c11b43db10cdd27560a896287140a65
BernardoMoschen <bernardomoschen.dev@gmail.com>
2021-08-09 21:01:59 -0300

    refactor: change func name to be more legible

--- Javascript/javascriptCRUD/projeto_inicial/admin/controller/atualizaCliente_Controller.js
+++ Javascript/javascriptCRUD/projeto_inicial/admin/controller/atualizaCliente_Controller.js
@@ -1,19 +1,20 @@
 // eslint-disable-next-line import/extensions
 import { detalhaCliente, atualizaCliente } from '../service/cliente_service.js';
 
-const id = new URL(window.location).searchParams.get('id');
-const inputNome = document.querySelector('[data-nome]');
-const inputEmail = document.querySelector('[data-email]');
+(async () => {
+  const id = new URL(window.location).searchParams.get('id');
+  const inputNome = document.querySelector('[data-nome]');
+  const inputEmail = document.querySelector('[data-email]');
 
-detalhaCliente(id).then((dados) => {
+  const dados = await detalhaCliente(id);
   inputNome.value = dados.nome;
   inputEmail.value = dados.email;
-});
 
-const formulario = document.querySelector('[data-form]');
-formulario.addEventListener('submit', (evento) => {
-  evento.preventDefault();
-  atualizaCliente(id, inputNome.value, inputEmail.value).then(() => {
+  const formulario = document.querySelector('[data-form]');
+
+  formulario.addEventListener('submit', async (evento) => {
+    evento.preventDefault();
+    await atualizaCliente(id, inputNome.value, inputEmail.value);
     window.location.href = '../telas/edicao_concluida.html';
   });
-});
+})();
