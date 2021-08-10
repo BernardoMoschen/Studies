ee1e263fd054313283bc4b9d241b181dd104cd59
BernardoMoschen <bernardomoschen.dev@gmail.com>
2021-08-09 21:27:58 -0300

    feat(attCliente_C):add error treatment

--- Javascript/javascriptCRUD/projeto_inicial/admin/controller/atualizaCliente_Controller.js
+++ Javascript/javascriptCRUD/projeto_inicial/admin/controller/atualizaCliente_Controller.js
@@ -5,16 +5,24 @@ import { detalhaCliente, atualizaCliente } from '../service/cliente_service.js';
   const id = new URL(window.location).searchParams.get('id');
   const inputNome = document.querySelector('[data-nome]');
   const inputEmail = document.querySelector('[data-email]');
-
-  const dados = await detalhaCliente(id);
-  inputNome.value = dados.nome;
-  inputEmail.value = dados.email;
+  try {
+    const dados = await detalhaCliente(id);
+    inputNome.value = dados.nome;
+    inputEmail.value = dados.email;
+  } catch (erro) {
+    console.log(erro);
+    window.location.href = '../telas/erro.html';
+  }
 
   const formulario = document.querySelector('[data-form]');
-
   formulario.addEventListener('submit', async (evento) => {
     evento.preventDefault();
-    await atualizaCliente(id, inputNome.value, inputEmail.value);
-    window.location.href = '../telas/edicao_concluida.html';
+    try {
+      await atualizaCliente(id, inputNome.value, inputEmail.value);
+      window.location.href = '../telas/edicao_concluida.html';
+    } catch (erro) {
+      console.log(erro);
+      window.location.href = '../telas/erro.html';
+    }
   });
 })();
