1bedb342b120f7e4146907ec1fd0c5d5d0e530fd
BernardoMoschen <bernardomoschen.dev@gmail.com>
2021-08-09 21:05:08 -0300

    refactor: now uses async await

--- Javascript/javascriptCRUD/projeto_inicial/admin/controller/cadastraClientes_Controller.js
+++ Javascript/javascriptCRUD/projeto_inicial/admin/controller/cadastraClientes_Controller.js
@@ -3,11 +3,11 @@ import { criaCliente } from '../service/cliente_service.js';
 
 const formulario = document.querySelector('[data-form]');
 
-formulario.addEventListener('submit', (evento) => {
+formulario.addEventListener('submit', async (evento) => {
   evento.preventDefault();
   const nome = evento.target.querySelector('[data-nome]').value;
   const email = evento.target.querySelector('[data-email]').value;
 
-  criaCliente(nome, email)
-    .then(() => { window.location.href = '../telas/cadastro_concluido.html'; });
+  await criaCliente(nome, email);
+  window.location.href = '../telas/cadastro_concluido.html';
 });
