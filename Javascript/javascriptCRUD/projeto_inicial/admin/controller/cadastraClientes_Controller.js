19b557a2513890e08ac09e3b6bddda54bb10b2f9
BernardoMoschen <bernardomoschen.dev@gmail.com>
2021-08-09 21:28:27 -0300

    feat(cadastraCliente_Cttrl):add error treatment

--- Javascript/javascriptCRUD/projeto_inicial/admin/controller/cadastraClientes_Controller.js
+++ Javascript/javascriptCRUD/projeto_inicial/admin/controller/cadastraClientes_Controller.js
@@ -5,9 +5,13 @@ const formulario = document.querySelector('[data-form]');
 
 formulario.addEventListener('submit', async (evento) => {
   evento.preventDefault();
-  const nome = evento.target.querySelector('[data-nome]').value;
-  const email = evento.target.querySelector('[data-email]').value;
-
-  await criaCliente(nome, email);
-  window.location.href = '../telas/cadastro_concluido.html';
+  try {
+    const nome = evento.target.querySelector('[data-nome]').value;
+    const email = evento.target.querySelector('[data-email]').value;
+    await criaCliente(nome, email);
+    window.location.href = '../telas/cadastro_concluido.html';
+  } catch (erro) {
+    console.log(erro);
+    window.location.href = '../telas/erro.html';
+  }
 });
