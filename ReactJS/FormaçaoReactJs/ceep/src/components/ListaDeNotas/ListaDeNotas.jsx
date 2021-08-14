004b86b2dd2af3a48a44c3c1fffb4cce23356873
BernardoMoschen <bernardomoschen.dev@gmail.com>
2021-08-14 11:45:04 -0300

    feat: updates ListaDeNotas

--- ReactJS/FormaçaoReactJs/ceep/src/components/ListaDeNotas/ListaDeNotas.jsx
+++ ReactJS/FormaçaoReactJs/ceep/src/components/ListaDeNotas/ListaDeNotas.jsx
@@ -1,22 +1,24 @@
+import React, { Component } from 'react';
+import CardNota from '../CardNota';
+import './estilo.css';
 
-import React, { Component } from "react";
-import CardNota from "../CardNota";
-import "./estilo.css";
 class ListaDeNotas extends Component {
- 
   render() {
     return (
       <ul className="lista-notas">
-        {this.props.notas.map((nota, index) => {
-          return (
-            <li className="lista-notas_item" key={index}>
-              <CardNota titulo={nota.titulo} texto={nota.texto}/>
-            </li>
-          );
-        })}
+        {this.props.notas.map((nota, index) => (
+          <li className="lista-notas_item" key={index}>
+            <CardNota  
+            indice={index}
+            deletarNota={this.props.deletarNota}
+            titulo={nota.titulo}
+            texto={nota.texto}
+            categoria={nota.categoria} />
+          </li>
+        ))}
       </ul>
     );
   }
 }
 
-export default ListaDeNotas
+export default ListaDeNotas;
