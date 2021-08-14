d109a62c254f1d91cccc183cf322ab5b1cfd18f3
BernardoMoschen <bernardomoschen.dev@gmail.com>
2021-08-14 11:45:26 -0300

    feat: updates FormularioCadastro

--- ReactJS/FormaçaoReactJs/ceep/src/components/FormularioCadastro/FormularioCadastro.jsx
+++ ReactJS/FormaçaoReactJs/ceep/src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -4,8 +4,9 @@ class FormularioCadastro extends Component {
 
   constructor(props){
     super(props);
-    this.titulo ="";
-    this.texto ="";
+    this.titulo = "";
+    this.texto = "";
+    this.categoria = "Sem categoria"
   }
 
   _handleMudancaTitulo(evento){
@@ -21,7 +22,7 @@ class FormularioCadastro extends Component {
   _criarNota(evento){
     evento.preventDefault();
     evento.stopPropagation();
-    this.props.criarNota(this.titulo, this.texto);
+    this.props.criarNota(this.titulo, this.texto, this.categoria);
     
   }
 
@@ -30,6 +31,12 @@ class FormularioCadastro extends Component {
       <form className="form-cadastro"
         onSubmit={this._criarNota.bind(this)}
       >
+        <select className="form-cadastro_input">
+          <option>Sem categoria</option>
+          {this.props.categorias.map((categoria) => {
+            return <option>{categoria}</option>
+          })}
+        </select>
         <input
           type="text"
           placeholder="Título"
