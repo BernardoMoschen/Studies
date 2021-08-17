ce53b75c788baee8cc78171a4d43f3c59bac8bb8
BernardoMoschen <bernardomoschen.dev@gmail.com>
2021-08-16 21:32:25 -0300

    refactor: data is now in separated class

--- ReactJS/FormaçaoReactJs/ceep/src/App.js
+++ ReactJS/FormaçaoReactJs/ceep/src/App.js
@@ -8,35 +8,7 @@ import './assets/index.css';
 class App extends Component {
   constructor() {
     super();
-
-    this.state = {
-      notas: [],
-      categorias: []
-    };
   }
-
-  criarNota(titulo, texto, categoria) {
-    const novaNota = { titulo, texto, categoria };
-    const novoArrayNotas = [...this.state.notas, novaNota];
-      const novoEstado = {
-      notas: novoArrayNotas,
-    };    
-    this.setState(novoEstado);
-  }
-
-  deletarNota(index) {
-    let arrayNotas = this.state.notas;
-    arrayNotas.splice(index, 1);
-    this.setState({notas:arrayNotas})
-    console.log('deletar');
-  }
-
-  adicionarCategoria(nomeCategoria){
-    const novoArrayCategorias = [...this.state.categorias, nomeCategoria]
-    const novoEstado = {...this.state, categorias:novoArrayCategorias}
-    this.setState(novoEstado)
-  }
-
   render() {
     return (   
       <section className="conteudo">
