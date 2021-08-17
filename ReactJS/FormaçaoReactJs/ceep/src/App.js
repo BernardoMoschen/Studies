bb881757b93ac91b776881ee189a46311d761d97
BernardoMoschen <bernardomoschen.dev@gmail.com>
2021-08-16 22:37:58 -0300

    final commit

--- ReactJS/FormaçaoReactJs/ceep/src/App.js
+++ ReactJS/FormaçaoReactJs/ceep/src/App.js
@@ -1,31 +1,32 @@
-import React, { Component } from 'react';
-import ListaDeNotas from './components/ListaDeNotas';
-import FormularioCadastro from './components/FormularioCadastro';
-import ListaDeCategorias from './components/ListaDeCategorias';
-import './assets/App.css';
-import './assets/index.css';
-import Categorias from './dados/Categorias';
-import ArrayDeNotas from './dados/Notas';
-
+import React, { Component } from "react";
+import ListaDeNotas from "./components/ListaDeNotas";
+import FormularioCadastro from "./components/FormularioCadastro";
+import ListaDeCategorias from "./components/ListaDeCategorias";
+import "./assets/App.css";
+import "./assets/index.css";
+import Categorias from "./dados/Categorias";
+import ArrayDeNotas from "./dados/Notas";
 class App extends Component {
   constructor() {
     super();
     this.categorias = new Categorias();
     this.notas = new ArrayDeNotas();
   }
+
   render() {
-    return (   
+    return (
       <section className="conteudo">
         <FormularioCadastro
-          categorias={this.categorias.categorias}
-          criarNota={this.notas.criarNota}
+          categorias={this.categorias}
+          criarNota={this.notas.adicionarNota.bind(this.notas)}
         />
         <main className="conteudo-principal">
-          <ListaDeCategorias 
+          <ListaDeCategorias
             adicionarCategoria={this.categorias.adicionarCategoria.bind(this.categorias)}
-            categorias={this.categorias}/>
+            categorias={this.categorias}
+          />
           <ListaDeNotas
-            deletarNota={this.notas.deletarNota.bind(this.notas)}
+            apagarNota={this.notas.apagarNota.bind(this.notas)}
             notas={this.notas}
           />
         </main>
@@ -34,5 +35,5 @@ class App extends Component {
   }
 }
 
-// new ListaDeNotas({notas:this.notas})
+//new ListaDeNotas({notas:this.notas})
 export default App;
