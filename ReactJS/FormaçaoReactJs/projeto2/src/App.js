234942ee2752e261582296454f7f214c0b366196
BernardoMoschen <bernardomoschen.dev@gmail.com>
2021-08-17 19:34:25 -0300

    First commit

---
+++ ReactJS/FormaçaoReactJs/projeto2/src/App.js
@@ -0,0 +1,15 @@
+import React, { Fragment } from 'react'
+
+import './App.css';
+import FormularioCadastro from './components/FormularioCadastro/FormularioCadastro';
+
+function App() {
+  return (
+    <Fragment>
+      <h1>Formulário Cadastro</h1>
+      <FormularioCadastro/>
+    </Fragment>
+  );
+}
+
+export default App;
