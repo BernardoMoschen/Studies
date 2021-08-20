50edf51dd1ba5f89ed40c259a0d3c0b34384a79b
BernardoMoschen <bernardomoschen.dev@gmail.com>
2021-08-20 20:47:30 -0300

    feat: form now changes dinamically

--- ReactJS/FormaçaoReactJs/projeto2/src/components/FormularioCadastro/FormularioCadastro.jsx
+++ ReactJS/FormaçaoReactJs/projeto2/src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { PropTypes } from 'prop-types';
+import { Typography } from '@material-ui/core';
 import DadosPessoais from './DadosPessoais';
 import DadosUsuario from './DadosUsuario';
 import DadosEntrega from './DadosEntrega';
 
 function FormularioCadastro({ aoEnviar, validarCPF }) {
+  const [etapaAtual, setEtapaAtual] = useState(0);
+
+  function formularioAtual(etapa) {
+    switch (etapa) {
+      case 0:
+        return <DadosUsuario aoEnviar={aoEnviar} validarCPF={validarCPF} />;
+      case 1:
+        return <DadosPessoais />;
+      case 2:
+        return <DadosEntrega />;
+      default:
+        return <Typography>Erro durante a seleção do formulário.</Typography>;
+    }
+  }
+
   return (
     <>
-      <DadosPessoais validarCPF={validarCPF} aoEnviar={aoEnviar} />
-      <DadosUsuario />
-      <DadosEntrega />
+      {formularioAtual(etapaAtual)}
     </>
   );
 }
