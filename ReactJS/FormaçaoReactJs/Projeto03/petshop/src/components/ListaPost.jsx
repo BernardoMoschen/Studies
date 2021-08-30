78b0c8ac8c1a58a78aa6f572f7cff79fadea6254
BernardoMoschen <bernardomoschen.dev@gmail.com>
2021-08-30 20:03:50 -0300

    feat: implements api

---
+++ ReactJS/FormaçaoReactJs/Projeto03/petshop/src/components/ListaPost.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { busca } from '../api/api'
+export default function () {
+    return (
+        <section className="posts container">
+            
+        </section >
+    )
+}
