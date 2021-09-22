bbd5fa5e25bf60dc31bdeec70a57958f97b9394d
BernardoMoschen <bernardomoschen.dev@gmail.com>
2021-09-22 10:57:57 -0300

    eat: adds function test case

--- src/App.test.js
+++ src/App.test.js
@@ -1,22 +1,33 @@
 import React from 'react'
-import App from './App'
+import App, {calcularNovoSaldo} from './App'
 import {render, screen} from '@testing-library/react'
 
-describe('Main component', () => {
-    describe('When i open the app,', () => {
-        it('the bank name is displayed', () => {
+describe('Componente principal', () => {
+    describe('Quando eu abro o App,', () => {
+        it('O nome do banco e exibido', () => {
             render(<App/>);
             expect(screen.getByText('ByteBank')).toBeInTheDocument()
         })
     
-        it('my balance is displayed', () => {
+        it('Meu saldo e exibido', () => {
             render(<App/>);
             expect(screen.getByText('Saldo:')).toBeInTheDocument()
         })
     
-        it('the perform transaction button is displayed', () => {
+        it('O botao de realizar transacao e exibido', () => {
             render(<App/>);
             expect(screen.getByText('Realizar operação')).toBeInTheDocument()
         }) 
+    })
+
+    describe('Quando eu realizado uma transacao,', () => {
+        it('de saque, o valor diminui', () => {
+            const valores = {
+                transacao: 'saque',
+                valor: 50
+            }
+            const novoSaldo = calcularNovoSaldo(valores, 150);
+            expect(novoSaldo).toBe(100)
+        })
     })
 })
