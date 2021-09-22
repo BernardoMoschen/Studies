182d3d4678a3f67fcd84d18ef9c3cdef1e25367e
BernardoMoschen <bernardomoschen.dev@gmail.com>
2021-09-22 18:39:53 -0300

    feat: creates a ItemDiv component

---
+++ ReactJS/FormaçaoReactJs/alura-styled-components-master/src/Components/Item/Item.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Item = styled.div`
+display: flex;
+flex-direction: column;
+`
+
+export default ({type, from, value}) => (
+  <Item>
+  <span>
+  {type}
+  </span>
+  <span>
+  {from}
+  </span>
+  <span>
+  {value}
+  </span>
+  </Item>
+  )
--- ReactJS/FormaçaoReactJs/alura-styled-components-master/src/Components/Items/index.jsx
+++ ReactJS/FormaçaoReactJs/alura-styled-components-master/src/Components/Items/index.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import styled from 'styled-components'
+import Item from '../Item/Item'
 
 const Items = styled.div`
 box-shadow: 4px 4px 20px 0px rgba(0, 0, 0, 0.1);
 border-radius: 10px;
 margin: 2px:
-display: flex;
 align-items: center;
+display: flex;
 justify-content: space-around;
 padding: 10px;
 font-size: 10px
 `
 
-export default () => <Items/>
+export default (props) => 
+  <Items>
+    <Item {...props}/>
+  </Items>
