c779a3bcffad776b8dc55703db4b79460ab90698
BernardoMoschen <bernardomoschen.dev@gmail.com>
2021-09-22 18:09:48 -0300

    refactor icon to make it only inherit props

--- ReactJS/FormaçaoReactJs/alura-styled-components-master/src/Components/Conta/index.jsx
+++ ReactJS/FormaçaoReactJs/alura-styled-components-master/src/Components/Conta/index.jsx
@@ -1,16 +1,20 @@
 import React, { useState } from "react";
-
+import styled from "styled-components";
 import privado from "../../assets/images/privado.svg";
 import olho_icone from "../../assets/images/olho.svg";
 import dinheiro from "../../assets/images/dinheiro.svg";
 import { Icone } from '../../Components/UI'
 
+const IconeMargin = styled(Icone)`
+margin-top: 2px
+`
 const Conta = () => {
   const [toggleState, untoggle] = useState(true);
 
   const toggleHandler = () => {
     untoggle((toggleState) => !toggleState);
   };
+
 
   return (
     <div className="box">
@@ -28,8 +32,7 @@ const Conta = () => {
       </div>
 
       <button className="btn" onClick={toggleHandler}>
-        <Icone
-          style={{ marginTop: "2px" }}
+        <IconeMargin
           src={toggleState ? privado : olho_icone}
           alt="Privacidade do Saldo"
         />
