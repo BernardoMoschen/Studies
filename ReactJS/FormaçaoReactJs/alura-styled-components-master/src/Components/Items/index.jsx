04571f241c3ac55cec8924ddb1e02965a4adc42c
BernardoMoschen <bernardomoschen.dev@gmail.com>
2021-09-22 18:33:00 -0300

    feat: creates a ItemsDiv component

---
+++ ReactJS/FormaçaoReactJs/alura-styled-components-master/src/Components/Items/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Items = styled.div`
+box-shadow: 4px 4px 20px 0px rgba(0, 0, 0, 0.1);
+border-radius: 10px;
+margin: 2px:
+display: flex;
+align-items: center;
+justify-content: space-around;
+padding: 10px;
+font-size: 10px
+`
+
+export default () => <Items/>
