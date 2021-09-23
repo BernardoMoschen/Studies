c6386a3126d558c434884e1541e3617811fd141e
BernardoMoschen <bernardomoschen.dev@gmail.com>
2021-09-22 21:57:23 -0300

    feat:items now uses ImageFilter

--- ReactJS/FormaçaoReactJs/alura-styled-components-master/src/Components/Items/index.jsx
+++ ReactJS/FormaçaoReactJs/alura-styled-components-master/src/Components/Items/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 import Item from '../Item/Item'
+import ImageFilter from '../ImageFIlter'
 
 const Items = styled.div`
 box-shadow: 4px 4px 20px 0px rgba(0, 0, 0, 0.1);
@@ -15,6 +16,7 @@ font-size: 10px
 
 export default (props) => 
   <Items>
+    {ImageFilter(props.type)}
     <Item {...props}/>
     <span>{props.date}</span>
   </Items>
